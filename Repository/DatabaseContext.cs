6ef625f8b5ca351d10f22295057525ef4eb90689
uafa6 <315211@via.dk>
2023-05-09 18:54:06 +0200

    Database connection (everyone contributed)

--- Repository/DatabaseContext.cs
+++ Repository/DatabaseContext.cs
@@ -10,21 +10,20 @@ public class DatabaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        /*optionsBuilder.UseNpgsql(
+        optionsBuilder.UseNpgsql(
             $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
             $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
             $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
             $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
-            $"Password={Environment.GetEnvironmentVariable("DB_PASS")}",
-            options => options.UseAdminDatabase(Environment.GetEnvironmentVariable("DB_ADMIN_DATABASE")));*/
+            $"Password={Environment.GetEnvironmentVariable("DB_PASS")}");
         
-        optionsBuilder.UseNpgsql("" +
+        /*optionsBuilder.UseNpgsql("" +
                                  DatabaseCredentials.Host +
                                  DatabaseCredentials.Port +
                                  DatabaseCredentials.Database +
                                  DatabaseCredentials.Username +
                                  DatabaseCredentials.Password,
-            options => options.UseAdminDatabase(DatabaseCredentials.AdminDatabase));
+            options => options.UseAdminDatabase(DatabaseCredentials.AdminDatabase));*/
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
