51a002371fa7b1885fc46f39ebf64fc435d2aa28
Bl1tzz18 <91396657+Bl1tzz18@users.noreply.github.com>
2023-05-14 20:53:28 +0200

    Terrarium added to Model

---
+++ Model/Terrarium.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model;
+
+public class Terrarium
+{
+    [Key]
+    public String name { get; set; }
+    public TerrariumBoundaries terrariumBoundaries;
+    public TerrariumLimits terrariumLimits;
+    public Measurements measurements;
+}
