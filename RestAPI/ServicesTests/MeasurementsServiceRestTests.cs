0786d8fad40f07bb9c9298f48dd36facbba3b26d
Bl1tzz18 <91396657+Bl1tzz18@users.noreply.github.com>
2023-05-17 22:26:25 +0200

    Setup XUnit for Testing (Rest side)

---
+++ RestAPI/ServicesTests/MeasurementsServiceRestTests.cs
@@ -0,0 +1,21 @@
+﻿using Model;
+using ReptiMate_Cloud.Services;
+using RestDAOs;
+using Moq;
+using Xunit;
+
+namespace ReptiMate_Cloud.ServicesTests;
+
+public class MeasurementsServiceRestTests
+{
+    private readonly MeasurementsServiceRest _measurementsService;
+    private readonly Mock<IRestMeasurementsDAO> _measurementsDaoMock;
+
+    
+    public MeasurementsServiceRestTests()
+    {
+        _measurementsDaoMock = new Mock<IRestMeasurementsDAO>();
+        _measurementsService = new MeasurementsServiceRest(_measurementsDaoMock.Object);
+    }
+    
+}
