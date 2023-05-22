f21e0d2118be4f81cefc91ddb5effe44d154b11c
Beatricia Nagit <315186@via.dk>
2023-05-22 15:52:12 +0200

    changing the value of the redirectUri for enabling the frontend team to use deployed server

--- Model/Dto/AuthenticationDto.cs
+++ Model/Dto/AuthenticationDto.cs
@@ -3,9 +3,11 @@
 public class AuthenticationDto
 {
     public string Code { get; set; }
+    public bool IsDevSource { get; set; }
 
-    public AuthenticationDto(string code)
+    public AuthenticationDto(string code, bool isDevSource)
     {
         Code = code;
+        IsDevSource = isDevSource;
     }
 }
